9c49e00842813f08d6092d74ed92e9b839ebef15
Sheryl Darroch <sdarroch@ibs365.com>
2022-07-12 12:41:04 -0400

    wip

--- src/webparts/powerBiEmbedReports/hooks/useGetReportConfig.tsx
+++ src/webparts/powerBiEmbedReports/hooks/useGetReportConfig.tsx
@@ -53,18 +53,23 @@ export const useGetReportConfig = () => {
     getReportConfigDispatch({ type: "FETCH_START" });
     let results: IReportsList[];
     let requestOptions: IHttpClientOptions;
-    const AzureFunctionUrl = 'https://powerbisharepoint.azurewebsites.net/api/GetToken?code=NT8rk9oNqDIVe92KsxQPodRrynS_4t9FeTxQK7WeNtPKAzFu-vzy8w==';
+    // const AzureFunctionUrl = 'https://powerbisharepoint.azurewebsites.net/api/GetToken?code=epmSB0fCWciQLsZ9R2AiaDBRw-eJYaA5wVH5npnw28liAzFudxQ9KQ==';
+    const AzureFunctionUrl = 'https://maryvill-test-function.azurewebsites.net/api/gettoken?code=oZN_P3a2R2zRuTBegu-rla57DETCkRJQFOlva3M_pv0EAzFua53Pew%3D%3D';
 
+    console.log(reports);
     let reportsString = JSON.stringify(reports);
+    console.log(reportsString);
 
     const requestUrl = AzureFunctionUrl;
+    console.log(requestUrl);
     const httpClientOptions: IHttpClientOptions = {
       body: reportsString
     }
 
     aadHttpClient.getClient(
       //This is the App's Client ID
-      'a0ac7405-5171-4909-8f8f-c195ab60c28d'
+      // 'a0ac7405-5171-4909-8f8f-c195ab60c28d'
+      '170af556-d26c-40b3-9a96-361ce11d683d'
     )
       .then((client: AadHttpClient): void => {
         client.post(
@@ -78,10 +83,12 @@ export const useGetReportConfig = () => {
               return response.json();
             }
             else {
+              console.log(response);
               throw "Token fetch request failed!";
             }
           })
           .then((jsonResponse: IReportsList[]): void => {
+            console.log(jsonResponse);
             getReportConfigDispatch({ type: 'FETCH_SUCCESS', payload: jsonResponse });
           })
           .catch(error => {
