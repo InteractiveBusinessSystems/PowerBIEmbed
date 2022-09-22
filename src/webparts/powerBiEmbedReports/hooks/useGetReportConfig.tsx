76f04ca7167f1b9a19f5548bc6a317b66015698d
Sheryl Darroch <sdarroch@ibs365.com>
2022-09-22 16:37:41 -0400

    wip

--- src/webparts/powerBiEmbedReports/hooks/useGetReportConfig.tsx
+++ src/webparts/powerBiEmbedReports/hooks/useGetReportConfig.tsx
@@ -53,53 +53,54 @@ export const useGetReportConfig = () => {
     getReportConfigDispatch({ type: "FETCH_START" });
     let results: IReportsList[];
     let requestOptions: IHttpClientOptions;
-    // const AzureFunctionUrl = 'https://powerbisharepoint.azurewebsites.net/api/GetToken?code=epmSB0fCWciQLsZ9R2AiaDBRw-eJYaA5wVH5npnw28liAzFudxQ9KQ==';
-    const AzureFunctionUrl = 'https://maryvill-test-function.azurewebsites.net/api/gettoken?code=oZN_P3a2R2zRuTBegu-rla57DETCkRJQFOlva3M_pv0EAzFua53Pew%3D%3D';
 
-    console.log(reports);
-    let reportsString = JSON.stringify(reports);
-    console.log(reportsString);
+    // // const AzureFunctionUrl = 'https://powerbisharepoint.azurewebsites.net/api/GetToken?code=epmSB0fCWciQLsZ9R2AiaDBRw-eJYaA5wVH5npnw28liAzFudxQ9KQ==';
+    // const AzureFunctionUrl = 'https://maryvill-test-function.azurewebsites.net/api/gettoken?code=oZN_P3a2R2zRuTBegu-rla57DETCkRJQFOlva3M_pv0EAzFua53Pew%3D%3D';
 
-    const requestUrl = AzureFunctionUrl;
-    console.log(requestUrl);
-    const httpClientOptions: IHttpClientOptions = {
-      body: reportsString
-    }
+    // console.log(reports);
+    // let reportsString = JSON.stringify(reports);
+    // console.log(reportsString);
 
-    aadHttpClient.getClient(
-      //This is the App's Client ID
-      // 'a0ac7405-5171-4909-8f8f-c195ab60c28d'
-      '170af556-d26c-40b3-9a96-361ce11d683d'
-    )
-      .then((client: AadHttpClient): void => {
-        client.post(
-          //Calling AzureFunction
-          requestUrl,
-          AadHttpClient.configurations.v1,
-          httpClientOptions
-        )
-          .then((response: HttpClientResponse): Promise<IReportsList[]> => {
-            if (response.status === 200) {
-              return response.json();
-            }
-            else {
-              console.log(response);
-              throw "Token fetch request failed!";
-            }
-          })
-          .then((jsonResponse: IReportsList[]): void => {
-            console.log(jsonResponse);
-            getReportConfigDispatch({ type: 'FETCH_SUCCESS', payload: jsonResponse });
-          })
-          .catch(error => {
-            console.log(error);
-            getReportConfigDispatch({ type: 'FETCH_ERROR', payload: error });
-          });
-      })
-      .catch(aadError => {
-        console.log(aadError);
-        getReportConfigDispatch({ type: 'FETCH_ERROR', payload: aadError });
-      });
+    // const requestUrl = AzureFunctionUrl;
+    // console.log(requestUrl);
+    // const httpClientOptions: IHttpClientOptions = {
+    //   body: reportsString
+    // }
+
+    // aadHttpClient.getClient(
+    //   //This is the App's Client ID
+    //   // 'a0ac7405-5171-4909-8f8f-c195ab60c28d'
+    //   '170af556-d26c-40b3-9a96-361ce11d683d'
+    // )
+    //   .then((client: AadHttpClient): void => {
+    //     client.post(
+    //       //Calling AzureFunction
+    //       requestUrl,
+    //       AadHttpClient.configurations.v1,
+    //       httpClientOptions
+    //     )
+    //       .then((response: HttpClientResponse): Promise<IReportsList[]> => {
+    //         if (response.status === 200) {
+    //           return response.json();
+    //         }
+    //         else {
+    //           console.log(response);
+    //           throw "Token fetch request failed!";
+    //         }
+    //       })
+    //       .then((jsonResponse: IReportsList[]): void => {
+    //         console.log(jsonResponse);
+    //         getReportConfigDispatch({ type: 'FETCH_SUCCESS', payload: jsonResponse });
+    //       })
+    //       .catch(error => {
+    //         console.log(error);
+    //         getReportConfigDispatch({ type: 'FETCH_ERROR', payload: error });
+    //       });
+    //   })
+    //   .catch(aadError => {
+    //     console.log(aadError);
+    //     getReportConfigDispatch({ type: 'FETCH_ERROR', payload: aadError });
+    //   });
 
   }, []);
   return { reportConfigState, getReportConfig };
