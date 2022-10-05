0ca1485c5fda93445ee440de5afa98646a10564e
Sheryl Darroch <sdarroch@ibs365.com>
2022-10-05 12:02:18 -0400

    wip

--- src/webparts/powerBiEmbedReports/components/App.tsx
+++ src/webparts/powerBiEmbedReports/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useReportsList } from '../hooks/useReportsList';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react';
 import { useGetReportConfig } from '../hooks/useGetReportConfig';
 import { models } from 'powerbi-client';
@@ -35,48 +35,47 @@ export const App = (props:IAppProps) => {
     }
   },[isAudienced, reportsListError, reportsListIsLoading, getReportConfig]);
 
-
-    if (!getReportConfigIsLoading && !getReportConfigError) {
-      reportsMap = ReportConfig.map((report) =>
-        <div>
-          <a
-            href={report.ReportUrl}
-            target="_blank"
-            className={styles.reportTitle}
-          >{report.ReportName}</a>
-          <PowerBIEmbed
-            embedConfig={{
-              type: 'report',   // Supported types: report, dashboard, tile, visual and qna
-              id: report.ReportId,
-              embedUrl: report.EmbedUrl,
-              accessToken: report.EmbedToken,
-              tokenType: models.TokenType.Embed,
-              settings: {
-                panes: {
-                  filters: {
-                    expanded: false,
-                    visible: true
-                  }
-                },
-                // background: models.BackgroundType.Transparent,
-              }
-            }}
-
-            eventHandlers={
-              new Map([
-                ['loaded', function () { console.log('Report loaded'); }],
-                ['rendered', function () { console.log('Report rendered'); }],
-                ['error', function (event) { console.log(event.detail); }]
-              ])
+  if (!getReportConfigIsLoading && !getReportConfigError) {
+    console.log(ReportConfig);
+    reportsMap = ReportConfig.map((report) =>
+      <div>
+        <a
+          href={report.ReportUrl}
+          target="_blank"
+          className={styles.reportTitle}
+        >{report.ReportName}</a>
+        <PowerBIEmbed
+          embedConfig={{
+            type: 'report',   // Supported types: report, dashboard, tile, visual and qna
+            id: report.ReportId,
+            embedUrl: report.EmbedUrl,
+            accessToken: report.AccessToken,
+            tokenType: models.TokenType.Aad,
+            settings: {
+              panes: {
+                filters: {
+                  expanded: false,
+                  visible: true
+                }
+              },
+              // background: models.BackgroundType.Transparent,
             }
+          }}
 
-            cssClassName={styles.embeddedReport}
+          eventHandlers={
+            new Map([
+              ['loaded', function () { console.log('Report loaded'); }],
+              ['rendered', function () { console.log('Report rendered'); }],
+              ['error', function (event) { console.log(event.detail); }]
+            ])
+          }
 
-          />
-        </div>
-      );
-    }
+          cssClassName={styles.embeddedReport}
 
+        />
+      </div>
+    );
+  }
 
   if (reportsListIsLoading) {
     return (
@@ -107,16 +106,45 @@ export const App = (props:IAppProps) => {
   }
   else {
     return (
-         <Carousel
-            buttonsLocation={CarouselButtonsLocation.bottom}
-            buttonsDisplay={CarouselButtonsDisplay.hidden}
-            contentContainerStyles={styles.carouselContent}
-            containerButtonsStyles={styles.carouselButtonsContainer}
-            indicators={true}
-            indicatorShape={CarouselIndicatorShape.square}
-            indicatorsDisplay={CarouselIndicatorsDisplay.block}
-            element={reportsMap}
-            interval={null}
+        //  <Carousel
+        //     buttonsLocation={CarouselButtonsLocation.bottom}
+        //     buttonsDisplay={CarouselButtonsDisplay.hidden}
+        //     contentContainerStyles={styles.carouselContent}
+        //     containerButtonsStyles={styles.carouselButtonsContainer}
+        //     indicators={true}
+        //     indicatorShape={CarouselIndicatorShape.square}
+        //     indicatorsDisplay={CarouselIndicatorsDisplay.block}
+        //     element={reportsMap}
+        //     interval={null}
+        //   />
+        <PowerBIEmbed
+            embedConfig={{
+              type: 'report',   // Supported types: report, dashboard, tile, visual and qna
+        id: '79505149-73b0-45d3-98f9-e49b8a2328e2',
+        embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=79505149-73b0-45d3-98f9-e49b8a2328e2&groupId=7af23086-163b-4747-bd1c-977d1830d59b&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLU5PUlRILUNFTlRSQUwtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQiLCJlbWJlZEZlYXR1cmVzIjp7Im1vZGVybkVtYmVkIjp0cnVlLCJ1c2FnZU1ldHJpY3NWTmV4dCI6dHJ1ZSwic2tpcFF1ZXJ5RGF0YVNhYVNFbWJlZCI6dHJ1ZSwic2tpcFF1ZXJ5RGF0YVBhYVNFbWJlZCI6dHJ1ZSwic2tpcFF1ZXJ5RGF0YUV4cG9ydFRvIjp0cnVlfX0%3d',
+        accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNGVjNTU0OTMtNmIxYy00NTY1LWE4NjgtMmFlOTQwODgyYzgyLyIsImlhdCI6MTY2NDk4Mzg1NSwibmJmIjoxNjY0OTgzODU1LCJleHAiOjE2NjQ5ODg2NDcsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUFoQkd5enBKam5PWENwWGZHZ1JZS3lXYmVJaGRkUUJnMXg5UXMzZ2ZjeFpmYXc4WE5yYWV4MVZuYUJlbnBzb3ErZlpGdkhlTVhERmdQSDRRZ2dxcUJ6d2pWV2psdGMxTHBVVUtNODBkSldOTT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiMTcwYWY1NTYtZDI2Yy00MGIzLTlhOTYtMzYxY2UxMWQ2ODNkIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJEYXJyb2NoIiwiZ2l2ZW5fbmFtZSI6IlNoZXJ5bCIsImlwYWRkciI6IjEwNy4xMjYuODEuMTAxIiwibmFtZSI6IlNoZXJ5bCBEYXJyb2NoIiwib2lkIjoiMjNhN2M3ZDgtMjMzMy00ZWExLWE0NDEtY2Y3OGU4NzE2YjQyIiwicHVpZCI6IjEwMDNCRkZEQUFENkExRjIiLCJyaCI6IjAuQVFnQWsxVEZUaHhyWlVXb2FDcnBRSWdzZ2drQUFBQUFBQUFBd0FBQUFBQUFBQUFJQU9nLiIsInNjcCI6IkRhdGFzZXQuUmVhZC5BbGwgUmVwb3J0LlJlYWQuQWxsIFdvcmtzcGFjZS5SZWFkLkFsbCIsInNpZ25pbl9zdGF0ZSI6WyJrbXNpIl0sInN1YiI6IlpqWW1nVlkxSnNjZmxGS0tHR2o3NlZUSXM1MkYzVkNYQ3RaX1oxMmIzVEkiLCJ0aWQiOiI0ZWM1NTQ5My02YjFjLTQ1NjUtYTg2OC0yYWU5NDA4ODJjODIiLCJ1bmlxdWVfbmFtZSI6IlNEYXJyb2NoQGliczM2NS5jb20iLCJ1cG4iOiJTRGFycm9jaEBpYnMzNjUuY29tIiwidXRpIjoiQVVVUXNtcDRQVUdBNTIzUXZvVmFBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiNjJlOTAzOTQtNjlmNS00MjM3LTkxOTAtMDEyMTc3MTQ1ZTEwIiwiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.m5VmAqH9Zx2MFzGZ9gkM2pz-XqdUnvH_-mGtz3sElgnKNpQ1Eo5wxyZp8NCVeVyRVQRGot-sFBXUtyNvdeHEsOzDqNRujdwbNibY9PbH14wnzlUpDTHion1ED9VVqrT1HAgQRpvjisbagT1FcKExSX_1MYPiiHGj8PCQUDCxOjRGCjnUyWJJGyYDvFkpAVjf47rrnTUTSw1kzIe9v6hBT7Inyq8sGZeBFOCqKdA_MBLZqkcml7sJN0_pid_SSyQn9A8jUyhZ0mN3Q1JiDKZU7xDDgbSKhPpEOG9qFlHgztlSrmHuodCUP6D4ExXm6AKO8DM9gLno8d1YC5RYCROcCw',
+              tokenType: models.TokenType.Aad,
+              settings: {
+                panes: {
+                  filters: {
+                    expanded: false,
+                    visible: true
+                  }
+                },
+                // background: models.BackgroundType.Transparent,
+              }
+            }}
+
+            eventHandlers={
+              new Map([
+                ['loaded', function () { console.log('Report loaded'); }],
+                ['rendered', function () { console.log('Report rendered'); }],
+                ['error', function (event) { console.log(event.detail); }]
+              ])
+            }
+
+            cssClassName={styles.embeddedReport}
+
           />
     )
   }
--- src/webparts/powerBiEmbedReports/hooks/useGetReportConfig.tsx
+++ src/webparts/powerBiEmbedReports/hooks/useGetReportConfig.tsx
@@ -60,7 +60,7 @@ export const useGetReportConfig = () => {
       clientId: '170af556-d26c-40b3-9a96-361ce11d683d',
       authority: 'https://login.microsoftonline.com/4ec55493-6b1c-4565-a868-2ae940882c82',
       // redirectUri: 'http://localhost:3000/blank.html'
-      redirectUri: 'http://localhost:3000/myapp'
+      // redirectUri: 'http://localhost:3000/myapp'
     }
   };
 
@@ -69,194 +69,120 @@ export const useGetReportConfig = () => {
 
   const getReportConfig = useCallback(async (aadHttpClient: AadHttpClientFactory, reports) => {
     getReportConfigDispatch({ type: "FETCH_START" });
-    let results: IReportsList[];
-    // let requestOptions: IHttpClientOptions;
-
-    // // const AzureFunctionUrl = 'https://powerbisharepoint.azurewebsites.net/api/GetToken?code=epmSB0fCWciQLsZ9R2AiaDBRw-eJYaA5wVH5npnw28liAzFudxQ9KQ==';
-    // const AzureFunctionUrl = 'https://maryvill-test-function.azurewebsites.net/api/gettoken?code=oZN_P3a2R2zRuTBegu-rla57DETCkRJQFOlva3M_pv0EAzFua53Pew%3D%3D';
-
-    // console.log(reports);
-    // let reportsString = JSON.stringify(reports);
-    // console.log(reportsString);
-
-    // const requestUrl = AzureFunctionUrl;
-    // console.log(requestUrl);
-    // const httpClientOptions: IHttpClientOptions = {
-    //   body: reportsString
-    // }
-
-    // aadHttpClient.getClient(
-    //   //This is the App's Client ID
-    //   // 'a0ac7405-5171-4909-8f8f-c195ab60c28d'
-    //   '170af556-d26c-40b3-9a96-361ce11d683d'
-    // )
-    //   .then((client: AadHttpClient): void => {
-    //     client.post(
-    //       //Calling AzureFunction
-    //       requestUrl,
-    //       AadHttpClient.configurations.v1,
-    //       httpClientOptions
-    //     )
-    //       .then((response: HttpClientResponse): Promise<IReportsList[]> => {
-    //         if (response.status === 200) {
-    //           return response.json();
-    //         }
-    //         else {
-    //           console.log(response);
-    //           throw "Token fetch request failed!";
-    //         }
-    //       })
-    //       .then((jsonResponse: IReportsList[]): void => {
-    //         console.log(jsonResponse);
-    //         getReportConfigDispatch({ type: 'FETCH_SUCCESS', payload: jsonResponse });
-    //       })
-    //       .catch(error => {
-    //         console.log(error);
-    //         getReportConfigDispatch({ type: 'FETCH_ERROR', payload: error });
-    //       });
-    //   })
-    //   .catch(aadError => {
-    //     console.log(aadError);
-    //     getReportConfigDispatch({ type: 'FETCH_ERROR', payload: aadError });
-    //   });
-
-    console.log(reports);
+    let results: IReportsList[] = [];
     let loginResponse;
-    const currentUser:any = await spfi(sp).web.currentUser();
-    const currentUserEmail:string = currentUser.Email;
-    console.log(currentUserEmail);
+    let accessToken = "";
+    const currentUser: any = await spfi(sp).web.currentUser();
+    const currentUserEmail: string = currentUser.Email;
 
     const silentRequest = {
       scopes: ["https://analysis.windows.net/powerbi/api/Report.Read.All"],
       loginHint: currentUserEmail
     };
 
-    let accessToken = "";
-
-
+    // Create MsalInstance
     const msalInstance = new msal.PublicClientApplication(msalConfig);
 
+    // Check if users is signedIn using SSO
     try {
       loginResponse = await msalInstance.ssoSilent(silentRequest);
-      console.log(loginResponse);
     }
     catch (err) {
       if (err instanceof InteractionRequiredAuthError) {
-        msalInstance.loginPopup(silentRequest).then(response =>{
+        msalInstance.loginPopup(silentRequest).then(response => {
           console.log(response);
           loginResponse = response;
         }).catch(error => {
           console.log(error);
-          getReportConfigDispatch({type: 'FETCH_ERROR', payload: error});
+          getReportConfigDispatch({ type: 'FETCH_ERROR', payload: error });
         });
       }
       else {
         console.log(err);
-        getReportConfigDispatch({type: 'FETCH_ERROR', payload: err});
+        getReportConfigDispatch({ type: 'FETCH_ERROR', payload: err });
       }
     }
 
-    // try {
-    //   const loginResponse = await msalInstance.loginPopup(silentRequest);
-    //   console.log(loginResponse);
-    // }
-    // catch (err) {
-    //   console.log(err);
-    //   getReportConfigDispatch({type: 'FETCH_ERROR', payload: err});
-    // }
+    if (loginResponse) {
 
+      //get access token from response
+      accessToken = loginResponse.accessToken;
 
-    if(loginResponse) {
-        // get access token silently
-        msalInstance.acquireTokenSilent(silentRequest).then((response) => {
-            //get access token from response
-            accessToken = response.accessToken;
-
-            // Power BI REST API call to refresh User Permissions in Power BI
-            // Refreshes user permissions and makes sure the user permissions are fully updated
-            // https://docs.microsoft.com/rest/api/power-bi/users/refreshuserpermissions
-            fetch("https://api.powerbi.com/v1.0/myorg/RefreshUserPermissions", {
-            headers: {
-                "Authorization": "Bearer " + accessToken
-            },
-            method: "POST"
-            })
-            .then(response => {
-                if (response.ok) {
-                        console.log("User permissions refreshed successfully.");
-                    } else {
-                        // Too many requests in one hour will cause the API to fail
-                        if (response.status === 429) {
-                            console.error("Permissions refresh will be available in up to an hour.");
-                        } else {
-                            console.error(response);
-                        }
-                    }
-            })
-            .catch(error => {
-                console.error("Failure in making API call." + error);
-            });
-
-            //Power BI REST API calls to get the embed URLs of the reports
-            reports.forEach(report => {
-                    fetch("https://api.powerbi.com/v1.0/myorg/groups/" + report.WorkspaceId + "/reports/" + report.ReportId, {
-                        headers: {
-                          "Authorization": "Bearer " + accessToken
-                        },
-                        method: "Get"
-                    }).then(response => {
-                        const errorMessage: string[] = [];
-                        errorMessage.push("Error occurred while fetching the embed URL of the report")
-                        errorMessage.push("Request Id: " + response.headers.get("requestId"));
-
-                        response.json().then(body => {
-                          if(response.ok){
-                            console.log(body["embedUrl"]);
-                            let embedUrl = body["embedUrl"];
-
-                            results.push({
-                                "ReportName": report.ReportName,
-                                "ReportId": report.ReportId,
-                                "Id": report.Id,
-                                "DataSetsId": report.DatasetsId,
-                                "WorkspaceId": report.WorkspaceId,
-                                "ReportUrl": report.ReportUrl,
-                                "UsersWhoCanView": report.UsersWhoCanView,
-                                "ViewerType": report.ViewerType,
-                                "AccessToken": accessToken,
-                                "EmbedUrl": embedUrl
-                            });
-                          }
-                          else {
-                            errorMessage.push("Error " + response.status + ": " + body.error.code);
-                            console.log(errorMessage);
-                            getReportConfigDispatch({type: 'FETCH_ERROR', payload: errorMessage});
-                          }
-                        })
-                        .catch(jsonError => {
-                            errorMessage.push("Error " + response.status + ": An error has occurred");
-                            console.log(errorMessage);
-                            getReportConfigDispatch({type: 'FETCH_ERROR', payload: errorMessage});
-                        });
-                    })
-                    .catch(embedError => {
-                        console.log(embedError);
-                        getReportConfigDispatch({type: 'FETCH_ERROR', payload: embedError});
-                    });
-
-                    return results;
-            });
-            getReportConfigDispatch({type: 'FETCH_SUCCESS', payload: results});
+      // Power BI REST API call to refresh User Permissions in Power BI
+      // Refreshes user permissions and makes sure the user permissions are fully updated
+      // https://docs.microsoft.com/rest/api/power-bi/users/refreshuserpermissions
+      fetch("https://api.powerbi.com/v1.0/myorg/RefreshUserPermissions", {
+        headers: {
+          "Authorization": "Bearer " + accessToken
+        },
+        method: "POST"
+      })
+        .then(response => {
+          if (response.ok) {
+            console.log("User permissions refreshed successfully.");
+          } else {
+            // Too many requests in one hour will cause the API to fail
+            if (response.status === 429) {
+              console.error("Permissions refresh will be available in up to an hour.");
+            } else {
+              console.error(response);
+            }
+          }
         })
-        .catch((mError: msal.AuthError) => {
-          if(mError.name === "InteractionRequiredAuthError"){
-            msalInstance.acquireTokenPopup(silentRequest);
-            // May need to add the Power BI Calls (173-247) here too
-          }
-          else {
-            getReportConfigDispatch({type: 'FETCH_ERROR', payload: mError});
-          }
+        .catch(error => {
+          console.error("Failure in making API call." + error);
         });
+
+      //Power BI REST API calls to get the embed URLs of the reports
+      reports.forEach(report => {
+        fetch("https://api.powerbi.com/v1.0/myorg/groups/" + report.WorkspaceId + "/reports/" + report.ReportId, {
+          headers: {
+            "Authorization": "Bearer " + accessToken
+          },
+          method: "Get"
+        }).then(response => {
+          const errorMessage: string[] = [];
+          // errorMessage.push("Error occurred while fetching the embed URL of the report")
+          // errorMessage.push("Request Id: " + response.headers.get("requestId"));
+
+          response.json().then(body => {
+            if (response.ok) {
+              let embedUrl = body["embedUrl"];
+
+              results.push({
+                "ReportName": report.ReportName,
+                "ReportId": report.ReportId,
+                "Id": report.Id,
+                "DataSetsId": report.DatasetsId,
+                "WorkspaceId": report.WorkspaceId,
+                "ReportUrl": report.ReportUrl,
+                "UsersWhoCanView": report.UsersWhoCanView,
+                "ViewerType": report.ViewerType,
+                "AccessToken": accessToken,
+                "EmbedUrl": embedUrl
+              });
+
+            }
+            else {
+              errorMessage.push("Error " + response.status + ": " + body.error.code);
+              console.log(errorMessage);
+              getReportConfigDispatch({ type: 'FETCH_ERROR', payload: errorMessage });
+            }
+          })
+            .catch(jsonError => {
+              errorMessage.push("Error " + response.status + ": An error has occurred");
+              console.log(errorMessage);
+              getReportConfigDispatch({ type: 'FETCH_ERROR', payload: errorMessage });
+            });
+        })
+          .catch(embedError => {
+            console.log(embedError);
+            getReportConfigDispatch({ type: 'FETCH_ERROR', payload: embedError });
+          });
+
+      });
+      console.log(results);
+      getReportConfigDispatch({ type: 'FETCH_SUCCESS', payload: results });
     }
     else {
       // user is not logged in or cached, you will need to log them in to acquire a token
